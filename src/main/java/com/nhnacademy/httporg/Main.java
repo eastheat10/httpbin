4dbcbf877de51a42cca0f4936d7840a22c136273
윤동열 <eastheat10@gmail.com>
2022-04-22 13:48:42 +0900

    modified jackson library

--- src/main/java/com/nhnacademy/httporg/Main.java
+++ src/main/java/com/nhnacademy/httporg/Main.java
@@ -1,10 +1,14 @@
 package com.nhnacademy.httporg;
 
+import com.nhnacademy.httporg.reponse.ResponseHeader;
 import com.nhnacademy.httporg.request.Request;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +23,11 @@ public class Main {
 
             Request request = new Request(socket);
             request.getRequest();
+
+            ResponseHeader header = new ResponseHeader(new HashMap<>(), socket);
+            String s = header.responseHeader();
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.write(s.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/nhnacademy/httporg/reponse/JsonDto.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.httporg.reponse;
+
+import java.util.Map;
+
+public class JsonDto {
+
+    Map<String, String> args;
+
+    String data;
+
+    String files;
+
+    String form;
+
+    Header header;
+
+    String json;
+    String origin;
+    String url;
+
+    public void t() {
+
+    }
+}
+
+class Header {
+
+}
