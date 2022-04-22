acd9d7e99916246c6568e7006648f246a3bf49d9
윤동열 <eastheat10@gmail.com>
2022-04-22 14:02:36 +0900

    confilct

--- src/main/java/com/nhnacademy/httporg/Main.java
+++ src/main/java/com/nhnacademy/httporg/Main.java
@@ -1,9 +1,17 @@
 package com.nhnacademy.httporg;
 
+<<<<<<< HEAD
+=======
+import com.nhnacademy.httporg.reponse.ResponseHeader;
+import com.nhnacademy.httporg.request.Request;
+import java.io.DataOutputStream;
+>>>>>>> 4dbcbf8 (modified jackson library)
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +23,7 @@ public class Main {
 
             String ip=(((InetSocketAddress) socket.getRemoteSocketAddress()).getAddress()).toString().replace("/","");
             System.out.println(ip);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
